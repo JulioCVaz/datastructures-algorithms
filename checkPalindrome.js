6c472b59e412ae963246f85a623c6c2938df8036
Júlio Vaz <julio.oliveiravaz0@gmail.com>
2020-01-05 21:12:46 -0300

    ajustado o checkPalindrome

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -1,18 +1,24 @@
 // Given the string, check if it is a palindrome.
 
 function checkPalindrome(inputString) {
-    if(inputString.length > 1){
-        let result = false;
-        for(let i = 0; i < inputString.length-1; i++){
-            if(inputString.charAt(i) == inputString.charAt((inputString.length)-(i+1))){
-                result = true;
-            }else{
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }else{
-        return true;
-    }
+    if(inputString === inputString.split("").reverse().join("")){return true} else { return false }
 }
+
+
+// ------- old - 16 jan 19
+// function checkPalindrome(inputString) {
+//     if(inputString.length > 1){
+//         let result = false;
+//         for(let i = 0; i < inputString.length-1; i++){
+//             if(inputString.charAt(i) == inputString.charAt((inputString.length)-(i+1))){
+//                 result = true;
+//             }else{
+//                 result = false;
+//                 break;
+//             }
+//         }
+//         return result;
+//     }else{
+//         return true;
+//     }
+// }
