ae502ba3edc0e07447fd94b6ac3fdc7bd8328499
Júlio Vaz <julio.oliveiravaz0@gmail.com>
2020-02-11 22:53:33 -0300

    [daily 01 - easy] challenge google

---
+++ daily_coding_problem/daily_coding_01.js
@@ -0,0 +1,13 @@
+// This problem was recently asked by Google.
+
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+// Bonus: Can you do this in one pass?
+
+function sumFirstAndLast(array){
+    return array[0] + array[array.length - 1];
+}
+
+sumManAndMin([10, 15, 3, 7]);
